fde70fdad25680872ae7990a4216fd49b9265e99
Thafsi PV <pvthafsi@gmail.com>
2023-10-12 13:25:00 +0530

    content

--- DE_Landing/src/components/Home2.jsx
+++ DE_Landing/src/components/Home2.jsx
@@ -7,18 +7,19 @@ import homeimg from "../assets/img/homeimg.png";
 import landinglogo from "../assets/img/landing-logo.webp";
 import feature2 from "../assets/img/feature2.png";
 import feature1 from "../assets/img/feature-1.png";
+import delogo from "../assets/img/delogo.png";
 
 function Home2() {
   return (
     <body>
-      <header class="fixed w-full">
-        <nav class="bg-white border-gray-200 py-2.5 ">
+      <header class="fixed w-full z-10 ">
+        <nav class=" border-gray-200 py-3.5 backdrop-blur-lg">
           <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
             <a href="#" class="flex items-center">
               <img
-                src="https://blog.resellerspanel.com/wp-content/uploads/2011/05/tld-de.png"
+                src={delogo}
                 class="h-6 mr-3 sm:h-9 shadow-green-400 drop-shadow-xl"
-                alt="Landwind Logo"
+                alt="dineease Logo"
               />
               <span class="self-center text-xl font-semibold whitespace-nowrap ">
                 DineEase
@@ -26,38 +27,11 @@ function Home2() {
             </a>
             <div class="flex items-center lg:order-2">
               <a
-                href="https://themesberg.com/product/tailwind-css/landing-page"
-                class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0   focus:outline-none ">
-                Download
+                target="_blank"
+                href="https://dine-ease-pos.onrender.com/"
+                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0   focus:outline-none ">
+                Try demo
               </a>
-              <button
-                data-collapse-toggle="mobile-menu-2"
-                type="button"
-                class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="mobile-menu-2"
-                aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg
-                  class="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill-rule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clip-rule="evenodd"></path>
-                </svg>
-                <svg
-                  class="hidden w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"></path>
-                </svg>
-              </button>
             </div>
             <div
               class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
@@ -75,14 +49,7 @@ function Home2() {
                   <a
                     href="#"
                     class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0  lg: ">
-                    Company
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0  lg: ">
-                    Marketplace
+                    Dashboard
                   </a>
                 </li>
                 <li>
@@ -96,13 +63,6 @@ function Home2() {
                   <a
                     href="#"
                     class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0  lg: ">
-                    Team
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0  lg: ">
                     Contact
                   </a>
                 </li>
@@ -110,16 +70,18 @@ function Home2() {
             </div>
           </div>
         </nav>
-      </header> 
+      </header>
 
       {/* <!-- Start block --> */}
       <section class="bg-white ">
         <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
           <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl p-3 text-green-800 bg-gradient-to-r from-green-500 to-white rounded-lg">
-              Elevate the Dining <br />
-              Experience.
+            <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-violet-500 max-w-2xl mb-4 text-4xl 
+            font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl py-3 text-green-800 rounded-lg">
+              Simplifying Restaurant <br />
+              Management.
             </h1>
+            
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">
               Streamline your restaurant operations with our user-friendly
               platform, offering effortless order management, payment
@@ -129,20 +91,27 @@ function Home2() {
             </p>
             <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
               <a
-                href="https://github.com/themesberg/landwind"
+                href=""
                 class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100  ">
                 <svg
                   class="w-4 h-4 mr-2 text-gray-500 "
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 496 512">
                   <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-                </svg>{" "}
+                </svg>
                 View on GitHub
               </a>
               <a
                 href="https://www.figma.com/community/file/1125744163617429490"
                 class="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 ">
-                {/* <svg class="w-4 h-4 mr-2" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 300" width="1667" height="2500"><style type="text/css">.st0{fill:#0acf83}.st1{fill:#a259ff}.st2{fill:#f24e1e}.st3{fill:#ff7262}.st4{fill:#1abcfe}</style><title>Figma.logo</title><desc>Created using Figma</desc><path id="path0_fill" class="st0" d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z"/><path id="path1_fill" class="st1" d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z"/><path id="path1_fill_1_" class="st2" d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z"/><path id="path2_fill" class="st3" d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z"/><path id="path3_fill" class="st4" d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"/></svg> Get Figma file */}
+                <span className="flex items-center">
+                  <img
+                    src={delogo}
+                    alt="df"
+                    class="w-4 h-4 mr-2 text-gray-500 "
+                  />{" "}
+                  Try Demo
+                </span>
               </a>
             </div>
           </div>
@@ -696,7 +665,7 @@ function Home2() {
           </div>
         </div>
       </footer>
-      <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+      {/* <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script> */}
     </body>
   );
 }
