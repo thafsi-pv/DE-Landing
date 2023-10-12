bb13df49da1987c3b0a9b52557dd9eb2dcac4f46
Thafsi PV <pvthafsi@gmail.com>
2023-10-12 10:37:23 +0530

    first commit

---
+++ DE_Landing/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./components/Home";
+import Home2 from "./components/Home2";
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className="text-3xl font-bold underline text-green-600 bg-green-600">Hello world!</h1> */}
+      {/* <Home /> */}
+      <Home2/>
+    </>
+  );
+}
+
+export default App;
